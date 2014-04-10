63a46aa1ca748e1775c7b1a8b7ec5c58f630fb58
Chutiwat Srisuntorn <chutiwat@khroton.com>
2014-04-10 15:23:11 +0700

    add API getTable

--- CallingReserveTable/Kruaku/src/com/reserve/api/GetAvailableTable.java
+++ CallingReserveTable/Kruaku/src/com/reserve/api/GetAvailableTable.java
@@ -2,6 +2,10 @@ package com.reserve.api;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 import javax.servlet.ServletException;
@@ -11,6 +15,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.google.gson.Gson;
+import com.reserve.database.DatabaseHelper;
 import com.reserve.database.model.TableData;
 
 /**
@@ -32,18 +37,44 @@ public class GetAvailableTable extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		 getTable();
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		int inputAmount = 0;
-		ArrayList<TableData> tableResult = new ArrayList<TableData>();
-		String stringQuery = "SELECT * FROM dinning_table WHERE (available = 0) AND (capacity < " + inputAmount + ");";
-		PrintWriter printWriter = response.getWriter();
 		
+	}
+	
+	void getTable() {
+		ArrayList<TableData> tableResult = new ArrayList<TableData>();
+
+		DatabaseHelper.getInstance().open();
+		Connection connection = DatabaseHelper.getInstance().getConnection();
+		String stringQuery = "SELECT * FROM dining_table";
+		try {
+			PreparedStatement prepareStatement = connection.prepareStatement(stringQuery);
+			ResultSet resultSet = prepareStatement.executeQuery();
+            while (resultSet.next()) {
+		    	TableData table = new TableData();
+		    	
+		        table.setNumber(resultSet.getString("number"));
+		        table.setCapacity(resultSet.getInt("capacity"));
+		        table.setStatus(resultSet.getInt("available"));
+		        
+		        System.out.println(table.getNumber());
+		        
+		        tableResult.add(table);
+		    }
+	        resultSet.close();
+	        prepareStatement.close();
+	        
+	        System.out.print("table count" + tableResult.size());
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}		
 	}
 
 }
