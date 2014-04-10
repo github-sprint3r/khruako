3413aa6abb99a1ba9b822fd32d31762d82e32735
sansanee <sansanee@iconext.co.th>
2014-04-10 16:25:22 +0700

    modify servlet

--- InventoryScrum/src/com/email/XXX.java
+++ InventoryScrum/src/com/email/XXX.java
@@ -1,9 +1,6 @@
 package com.email;
 
-
-
 import java.io.IOException;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,15 +8,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class EmailSenderServlet
+ * Servlet implementation class XXX
  */
-public class EmailSenderServlet extends HttpServlet {
+@WebServlet("/XXX")
+public class XXX extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public EmailSenderServlet() { 
+    public XXX() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -29,27 +27,13 @@ public class EmailSenderServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		
-		response.getWriter().print("dfdfgdfgdfg");
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String email = request.getParameter("Email");
-		String tableNo = request.getParameter("TableNo");
-		String queueNo = request.getParameter("QueueNo");
-		
-		com.email.EmailSender sendEmail = new com.email.EmailSender();
-		boolean isPassMail = sendEmail.sendEmail(email, queueNo, tableNo);
-		
-		String result = "FAIL";
-		if(isPassMail)
-			result = "SUCCESS";
-		
-		request.setAttribute("SendMailResult", result);
-		request.getRequestDispatcher("result.jsp").forward(request, response);
+		// TODO Auto-generated method stub
 	}
 
 }
